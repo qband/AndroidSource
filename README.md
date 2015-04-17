fb259091ec12b97fdc63f2429447f1599d835565
qband <q7band@gmail.com>
2015-04-17 22:45:36 +0800

    add shell script to pull down source code

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 android development environment
+
+The script 'getPlugins.sh' use sock5 proxy 'localhost:7070' by default.
+Use 'sh getPlugins.sh' to pull down the code.
--- getPlugins.sh
+++ getPlugins.sh
@@ -1,13 +1,21 @@
 #!/usr/bin/env bash
 
-prefix=https://android.googlesource.com
+# add git config to use sock5 proxy
+# reference: http://git-scm.com/docs/git-config
+git config --global http.proxy 'socks5://127.0.0.1:7070'
+git config --global https.proxy 'socks5://127.0.0.1:7070'
 
+# googlesource
+prefix=https://android.googlesource.com
+# IDE
 repo1=platform/tools/idea
+# sdk manager
 repo2=platform/tools/base
 
-# IDE
+# pull down the source code
 git clone $prefix/$repo1 $repo1
-
-# sdk manager
 git clone $prefix/$repo2 $repo2
 
+# remove git config
+git config --global --unset http.proxy
+git config --global --unset https.proxy
