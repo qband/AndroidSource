1c9aeb353f4f9d3efcfae727237cfda451e7eff0
qband <q7band@gmail.com>
2015-04-18 21:01:18 +0800

    add sdk modules

--- getPlugins.sh
+++ getPlugins.sh
@@ -9,12 +9,16 @@ git config --global https.proxy 'socks5://127.0.0.1:7070'
 prefix=https://android.googlesource.com
 # IDE
 repo1=platform/tools/idea
-# sdk manager
-repo2=platform/tools/base
+# sdk tools
+repo2=platform/sdk
+repo3=platform/tools/base
+repo4=platform/tools/swt
 
 # pull down the source code
 git clone $prefix/$repo1 $repo1
 git clone $prefix/$repo2 $repo2
+git clone $prefix/$repo3 $repo3
+git clone $prefix/$repo4 $repo4
 
 # remove git config
 git config --global --unset http.proxy
