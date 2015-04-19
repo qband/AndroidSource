46a700dd27e48008914415d2a7309f24280ab9e7
qband <q7band@gmail.com>
2015-04-19 09:24:36 +0800

    update shell scripts

--- getPlugins.sh
+++ getPlugins.sh
@@ -13,12 +13,23 @@ repo1=platform/tools/idea
 repo2=platform/sdk
 repo3=platform/tools/base
 repo4=platform/tools/swt
+repo9=platform/prebuilts/devtools
+# build tool
+repo5=platform/manifest
+repo6=platform/build
+repo7=platform/tools/build
+repo8=platform/tools/buildSrc
 
 # pull down the source code
 git clone $prefix/$repo1 $repo1
 git clone $prefix/$repo2 $repo2
 git clone $prefix/$repo3 $repo3
 git clone $prefix/$repo4 $repo4
+git clone $prefix/$repo5 $repo5
+git clone $prefix/$repo6 $repo6
+git clone $prefix/$repo7 $repo7
+git clone $prefix/$repo8 $repo8
+git clone $prefix/$repo9 $repo9
 
 # remove git config
 git config --global --unset http.proxy
---
+++ getSource.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# install Repo
+mkdir -p ~/bin
+PATH=~/bin:$PATH
+proxychains curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+
+# Initialize a Repo client
+WORKING_DIRECTORY=~/Documents/JetBrains/IDEA/AndroidSource
+mkdir -p $WORKING_DIRECTORY
+cd $WORKING_DIRECTORY
+# bring down the latest version of Repo & specify a URL for the manifest
+proxychains repo init -u https://android.googlesource.com/platform/manifest
+
+# pull down the Android source tree
+repo sync
