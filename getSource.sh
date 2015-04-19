b58d5df55db609be1bcb77a879f46d470ee9e4f1
qband <q7band@gmail.com>
2015-04-19 09:44:35 +0800

    update shell scripts

--- getSource.sh
+++ getSource.sh
@@ -7,9 +7,9 @@ proxychains curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/
 chmod a+x ~/bin/repo
 
 # Initialize a Repo client
-WORKING_DIRECTORY=~/Documents/JetBrains/IDEA/AndroidSource
-mkdir -p $WORKING_DIRECTORY
-cd $WORKING_DIRECTORY
+#WORKING_DIRECTORY=~/Documents/JetBrains/IDEA/AndroidSource
+#mkdir -p $WORKING_DIRECTORY
+#cd $WORKING_DIRECTORY
 # bring down the latest version of Repo & specify a URL for the manifest
 proxychains repo init -u https://android.googlesource.com/platform/manifest
 
