a2d232a0d750aaf24b19aedad28ce0ba5afd23d8
qband <q7band@gmail.com>
2015-04-25 16:49:22 +0800

    update windows support

--- README.md
+++ README.md
@@ -7,8 +7,6 @@ proxychains sh get*Source.sh
 ##Windows
 use cntlm to solve GFW problem,
 please run in Git Bash:
-export HTTP_PROXY=127.0.0.1:3128
-export HTTPS_PROXY=127.0.0.1:3128
 get*SourceWin.sh
 
 #dependencies
--- getSysSourceWin.sh
+++ getSysSourceWin.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Set proxy
+export HTTP_PROXY=127.0.0.1:3128
+export HTTPS_PROXY=127.0.0.1:3128
+
 # Initialize a Repo client
 # bring down the latest version of Repo & specify a URL for the manifest
 repo init -u https://android.googlesource.com/platform/manifest
